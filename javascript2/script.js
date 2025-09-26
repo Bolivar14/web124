a0bad67d324b71d2f9c8ac621153979e36e7c6a2
Bolivar14 <andreinabolivarcampbell@gmail.com>
2025-09-25 22:15:29 -0500

    Add files via upload

---
+++ javascript2/script.js
@@ -0,0 +1,39 @@
+ /*Andreina Bolivar-Campbell 9/25/25, Java30 Project 2*/
+/*added current Date, and console log*/ 
+
+
+ const secondHand = document.querySelector('.second-hand');
+  const minsHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
+
+  function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  }
+
+  setInterval(setDate, 1000);
+
+  setDate();
+console.log("Clock is working");
+
+// New code to display the current date
+   const dateDisplayElement = document.getElementById('date');
+    const now = new Date();
+    const formattedDate = now.toLocaleDateString('en-US', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+    dateDisplayElement.textContent = formattedDate;
